04f298acd0f4860e25fa5930b686a6d3fd7d12b7
4cheedar <81338686+4Cheedar@users.noreply.github.com>
2023-03-01 09:33:57 -0300

    [UPD] Loja de Wallpaper

--- Commands/Info/shop.js
+++ Commands/Info/shop.js
@@ -2,11 +2,20 @@ const { SlashCommandBuilder, AttachmentBuilder } = require("discord.js");
 const CurrencySystem = require("currency-system");
 const cs = new CurrencySystem();
 const { createCanvas, Canvas, loadImage, registerFont } = require("canvas");
+const fs = require("fs");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("loja")
-    .setDescription("Mostra os itens da Loja"),
+    .setDescription("Mostra os itens da Loja")
+    .addSubcommand((options) =>
+      options.setName("items").setDescription("Mostra a loja de Itens")
+    )
+    .addSubcommand((options) =>
+      options
+        .setName("wallpapers")
+        .setDescription("Mostra a loja de Wallpapers")
+    ),
   /**
    *
    * @param {ChatInputCommandInteraction} interaction
@@ -14,64 +23,130 @@ module.exports = {
    */
   async execute(interaction) {
     await interaction.deferReply();
-    let result = await cs.getShopItems({
-      guild: interaction.guild,
-    });
+    const subCommand = interaction.options.getSubcommand();
+    const wallpapersPaste = "./CurrencyImages/BackgroundsShop";
 
     const canvas = createCanvas(800, 450);
     const ctx = canvas.getContext("2d");
-
-    const planoFundo = await loadImage(
-      "./CurrencyImages/CurrencySystem-shop.png"
-    );
 
     registerFont("./CurrencyImages/Fonts/Minecraftia-Regular.ttf", {
       family: "Minecraftia",
     });
 
-    ctx.drawImage(planoFundo, 0, 0, canvas.width, canvas.height);
+    switch (subCommand) {
+      case "items":
+        let result = await cs.getShopItems({
+          guild: interaction.guild,
+        });
 
-    let pos1 = 18;
-    let pos2 = 95;
-    let posDin = 205;
-    let posID = 120;
+        const planoFundoItems = await loadImage(
+          "./CurrencyImages/CurrencySystem-shop.png"
+        );
 
-    for (let key in result.inventory) {
-      let itemAdd = await loadImage(
-        `./CurrencyImages/ItensLoja/${result.inventory[key].name}.png`
-      );
+        ctx.drawImage(planoFundoItems, 0, 0, canvas.width, canvas.height);
 
-      ctx.drawImage(itemAdd, pos1, pos2, 75, 75);
+        let pos1 = 18;
+        let pos2 = 95;
+        let posDin = 205;
+        let posID = 120;
 
-      ctx.font = '20px "Minecraftia"';
-      ctx.fillStyle = "#228B22";
-      ctx.fillText(`$ ${result.inventory[key].price}`, pos1, posDin);
+        for (let key in result.inventory) {
+          let itemAdd = await loadImage(
+            `./CurrencyImages/ItensLoja/${result.inventory[key].name}.png`
+          );
 
-      ctx.font = '20px "Minecraftia"';
-      ctx.fillStyle = "#FFFFFF";
-      ctx.fillText(`${parseInt(key) + 1}`, pos1, posID);
+          ctx.drawImage(itemAdd, pos1, pos2, 75, 75);
 
-      if (key == 2 || key == 8 || key == 14) {
-        pos1 += 160;
-      } else if (key == 5) {
-        pos2 = 215;
-        pos1 = 18;
-        posDin = 325;
-        posID = 242;
-      } else if (key == 11) {
-        pos2 = 335;
-        pos1 = 18;
-        posDin = 447;
-        posID = 360;
-      } else {
-        pos1 += 130;
-      }
+          ctx.font = '20px "Minecraftia"';
+          ctx.fillStyle = "#228B22";
+          ctx.fillText(`$ ${result.inventory[key].price}`, pos1, posDin);
+
+          ctx.font = '20px "Minecraftia"';
+          ctx.fillStyle = "#FFFFFF";
+          ctx.fillText(`${parseInt(key) + 1}`, pos1, posID);
+
+          if (key == 2 || key == 8 || key == 14) {
+            pos1 += 160;
+          } else if (key == 5) {
+            pos2 = 215;
+            pos1 = 18;
+            posDin = 325;
+            posID = 242;
+          } else if (key == 11) {
+            pos2 = 335;
+            pos1 = 18;
+            posDin = 447;
+            posID = 360;
+          } else {
+            pos1 += 130;
+          }
+        }
+
+        var imgShop = new AttachmentBuilder(canvas.toBuffer(), "ShopItens.png");
+
+        return interaction.editReply({
+          files: [imgShop],
+        });
+
+        break;
+
+      case "wallpapers":
+        const planoFundoWallpapers = await loadImage(
+          "./CurrencyImages/CurrencySystem-shopWallpaper.png"
+        );
+        ctx.drawImage(planoFundoWallpapers, 0, 0, canvas.width, canvas.height);
+
+        let posOne = 85;
+        let posTwo = 50;
+        let posLado = 90;
+        let positionID = 85;
+        let posMoney = 155;
+
+        fs.readdir(wallpapersPaste, async function (err, files) {
+          for (key in files) {
+            var bg = await loadImage(
+              `./CurrencyImages/BackgroundsShop/${files[key]}`
+            );
+
+            if (files[key] !== "bg-default.png") {
+              ctx.drawImage(bg, posOne, posTwo, 200, 100);
+
+              ctx.font = '20px "Minecraftia"';
+              ctx.fillStyle = "#FFFFFF";
+              ctx.fillText(parseInt(key) + 1, posLado, positionID, 200, 100);
+
+              ctx.font = '20px "Minecraftia"';
+              ctx.fillStyle = "#FFFFFF";
+              ctx.fillText(`$ 25K`, posLado, posMoney);
+            }
+
+            if (key == 2) {
+              posOne = 85;
+              posTwo = 175;
+              posLado = 90;
+              positionID = 210;
+              posMoney = 280;
+            } else if (key == 5) {
+              posTwo = 300;
+              positionID = 335;
+              posLado = 90;
+              posOne = 85;
+              posMoney = 405;
+            } else {
+              posOne += 225;
+              posLado += 225;
+            }
+          }
+          var imgShopBG = new AttachmentBuilder(
+            canvas.toBuffer(),
+            "ShopBackgrounds.png"
+          );
+
+          return interaction.editReply({
+            files: [imgShopBG],
+          });
+        });
+        break;
     }
-
-    var imgShop = new AttachmentBuilder(canvas.toBuffer(), "ShopItens.png");
-
-    return interaction.editReply({
-      files: [imgShop],
-    });
   },
 };
